b0b73c87976d3a59748a4e32806b3e576d948635
Liam Landerloos <liam.landerloos@gmail.com>
2026-04-21 10:12:59 +0200

    Changed mudpot so it generates random redstone signals every 1 second

--- src/main/java/net/liamlanderloos/moreflowers/block/custom/MudPotBlock.java
+++ src/main/java/net/liamlanderloos/moreflowers/block/custom/MudPotBlock.java
@@ -2,28 +2,26 @@ package net.liamlanderloos.moreflowers.block.custom;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.world.level.block.entity.BlockEntityTicker;
-import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
-import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
-import java.util.logging.Level;
-
-public class MudPotBlock extends Block{
-    public static final BooleanProperty POWERED = BlockStateProperties.POWERED;
-    private static final VoxelShape SHAPE =
-            Block.box(5, 0, 5, 11, 6, 11);
+public class MudPotBlock extends Block {
+    public static final IntegerProperty POWER = BlockStateProperties.POWER;
+    private static final VoxelShape SHAPE = Block.box(5, 0, 5, 11, 6, 11);
+    private static final int TICK_RATE = 20; // 1 second in ticks
 
     public MudPotBlock(Properties properties) {
         super(properties);
-        this.registerDefaultState(this.stateDefinition.any().setValue(POWERED, true));
+        this.registerDefaultState(this.stateDefinition.any().setValue(POWER, 15));
     }
 
     @Override
@@ -41,19 +39,39 @@ public class MudPotBlock extends Block{
         return true;
     }
 
-    // Let redstone read from this block
     @Override
     public boolean isSignalSource(BlockState state) {
         return true;
     }
 
     @Override
-    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-        builder.add(POWERED);
+    protected int getSignal(BlockState blockState, BlockGetter blockAccess, BlockPos pos, Direction side) {
+        return blockState.getValue(POWER);
     }
 
     @Override
-    protected int getSignal(BlockState blockState, BlockGetter blockAccess, BlockPos pos, Direction side) {
-        return blockState.getValue(POWERED) ? 15 : 0;
+    public void onPlace(BlockState state, Level level, BlockPos pos, BlockState oldState, boolean isMoving) {
+        if (!level.isClientSide) {
+            level.scheduleTick(pos, this, TICK_RATE);
+        }
+    }
+
+    @Override
+    public void tick(BlockState state, ServerLevel level, BlockPos pos, RandomSource random) {
+        // Generate a random power between 0 and 15
+        // Using two random calls and taking the minimum can bias it towards lower values
+        int r1 = random.nextInt(16);
+        int r2 = random.nextInt(16);
+        int newPower = Math.min(r1, r2);
+
+        level.setBlock(pos, state.setValue(POWER, newPower), 3);
+        
+        // Schedule the next random update
+        level.scheduleTick(pos, this, TICK_RATE);
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(POWER);
     }
 }
